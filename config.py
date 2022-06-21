d8c8947cb7d259d8433736056175b376266e08ca
maarcweiss <106281165+maarcweiss@users.noreply.github.com>
2022-06-21 17:14:33 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# file used to store our API keys
-# insert de APIs from your own binace account
-# you have to set your own ones because they are private, don't show them to nowbody
-API_KEY = ""
-API_SECRET = ""
